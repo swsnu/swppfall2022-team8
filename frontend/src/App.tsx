7ba14a69ffe8381d393a57e74d079141dc3b16d8
ipomoea-nil <107753635+ipomoea-nil@users.noreply.github.com>
2022-10-20 13:10:11 +0900

    add frontend skeletons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import MainPage from './containers/MainPage/MainPage';
+import BookListPage from './containers/BookListPage/BookListPage';
+import BookRegisterPage from './containers/BookRegisterPage/BookRegisterPage';
+import BookDetailPage from './containers/BookDetailPage/BookDetailPage';
+import BookEditPage from './containers/BookEditPage/BookEditPage';
+import BookRequestPage from './containers/BookRequestPage/BookRequestPage';
+import ChattingPage from './containers/ChattingPage/ChattingPage';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/search/:key" element={<BookListPage />} />
+          <Route path="/book/register" element={<BookRegisterPage />} />
+          <Route path="/book/:id" element={<BookDetailPage />} />
+          <Route path="/book/:id/edit" element={<BookEditPage />} />
+          <Route path="/book/:id/request" element={<BookRequestPage />} />
+          <Route path="/chatting" element={<ChattingPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/containers/BookEditPage/BookEditPage.tsx
@@ -0,0 +1,9 @@
+const BookEditPage = () => {
+  return (
+    <>
+      <h1>BookEditPage</h1>
+    </>
+  );
+}
+
+export default BookEditPage
