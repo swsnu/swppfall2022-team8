911ea5ba0bf1f9b220954d4e02311ac75e0f9b8f
TaeYoung Rhee <61928634+SB1491@users.noreply.github.com>
2022-10-22 19:34:44 +0900

    Update BookRequestPage.tsx

--- frontend/src/containers/BookRequestPage/BookRequestPage.tsx
+++ frontend/src/containers/BookRequestPage/BookRequestPage.tsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { isTaggedTemplateExpression } from "typescript";
+
+const questionsList = [
+  "Question1",
+  "Qusetion2",
+]
+
 const BookRequestPage = () => {
+  const navigate = useNavigate();
+  const [answers, setAnswers] = useState<Array<string>>([...Array(questionsList.length)].map(() => ""));
+  const sendButtonHandler = () => {
+    navigate("/chatting");
+  }
+
+  const answerChangeHandler = (id: number, value: string) => {
+    const cpy = [...answers];
+    cpy[id] = value;
+    setAnswers(cpy);
+  }
+
   return (
     <>
-      <h1>BookRequestPage</h1>
+      <h1>Book Request Page</h1>
+      <h3>Name</h3>
+      example name 
+
+      {questionsList.map((q, i) => {
+        return(
+          <div key={`question_${i}`}>
+            <h3>{q}</h3>
+            <h3>Answer:</h3>
+            <input
+              value={answers[i]}
+              onChange={(event) => answerChangeHandler(i, event.target.value)}></input>
+          </div>
+        )
+      })}
+
+      <p></p>
+      <button onClick={() => sendButtonHandler()}>send to lender</button>
     </>
   );
 }
