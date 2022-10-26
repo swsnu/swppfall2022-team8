529494e7c625c18c22207d6bb8808fbc976252e1
ipomoea-nil <107753635+ipomoea-nil@users.noreply.github.com>
2022-10-26 20:31:38 +0900

    practice8 team task

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-Hoshimachi Suisei
+[![Build Status](https://travis-ci.com/swsnu/swppfall2022-team8.svg?branch=main)](https://travis-ci.com/swsnu/swppfall2022-team8)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=swsnu_swppfall2022-teamX&metric=alert_status)](https://sonarcloud.io/dashboard?id=swsnu_swppfall2022-teamX)
 
-Kurosawa Ruby
+# BookVillage
---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended', // This is for React
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'] // This is for Typescript
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
