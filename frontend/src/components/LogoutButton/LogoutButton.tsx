641814abc877f6e99b40937d89371b8e13ae25e7
SB1491 <etaeyoung2@snu.ac.kr>
2022-11-02 18:40:38 +0900

    navBar buttons css

--- frontend/src/components/LogoutButton/LogoutButton.tsx
+++ frontend/src/components/LogoutButton/LogoutButton.tsx
@@ -1,6 +1,10 @@
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../store'
 import { requestLogout } from '../../store/slices/user/user'
+import './LogoutButton.css'
 
 const LogoutButton = () => {
   const dispatch = useDispatch<AppDispatch>()
@@ -10,12 +14,18 @@ const LogoutButton = () => {
   }
 
   return (
-    <>
-      <button
+    <div className='logout'>
+      <Button
         type="button"
+        id="logout-button"
+        variant='light'
         onClick={() => clickLogoutHandler()}
-      >Logout</button>
-    </>
+      ><h5><FontAwesomeIcon
+          icon={faRightFromBracket}
+          id='logout-icon'
+        /></h5>
+      </Button>
+    </div>
   )
 }
 
