06e8c7d389556d2936bcde36f1a18425ac0016ee
ipomoea-nil <107753635+ipomoea-nil@users.noreply.github.com>
2022-12-12 14:26:31 +0900

    s -> S

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className='footer'>
       <br/>
       <br/>
-      Powered by BookVillage team, ipomoea-nil, src77, SB1491 and Pabiya,
+      Powered by BookVillage team, ipomoea-nil, Src77, SB1491 and Pabiya,
       <br/>
       with love for Hoshimachi Suisei and Kurosawa Ruby.
       <br/>
